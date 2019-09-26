1e82504f9eed47371226dceabe946604fb6c19d9
Livia Hilario <liviasantoshilario@gmail.com>
2019-09-26 16:01:34 -0300

    button com funcionalidade

--- CatYear/ViewController.swift
+++ CatYear/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var idadeGatoText: UITextField!
+    @IBOutlet weak var idadeCalculadaLabel: UILabel!
+    @IBAction func testButton(_ sender: Any) {
+      print("botao foi clicado")
 
+        if let idade = idadeGatoText.text {
+            if let idadeNumero = Int(idade) {
+                 let idadeGatoEmAnos = idadeNumero * 7
+                print("Aparece na label")
+                
+                idadeCalculadaLabel.text = "Seu gato tem " + String (idadeGatoEmAnos)+" na idade dos gatos"
+            }
+
+        }
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print (" Teste ")
+
     }
 
 
